c672f9a09940b9191dc50e374d7adb7c4fa0a193
Ubuntu <ubuntu@ip-172-31-83-87.ec2.internal>
2018-10-19 06:10:12 +0000

    working create passengers

--- createPassenger.php
+++ createPassenger.php
@@ -50,6 +50,28 @@
         }
 
 	// insert cool query stuff here rip
+	$db_file = './myDB/airport.db';
+
+	try {
+		// open connection to the database
+		$db = new PDO('sqlite:' . $db_file);
+
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$query_str = $db->prepare("INSERT into passengers (ssn, f_name, m_name, l_name) VALUES (:ssn, :f_name, :m_name, :l_name);");
+
+		$query_str->bindParam("ssn", $ssn);
+		$query_str->bindParam("f_name", $f_name);
+		$query_str->bindParam("m_name", $m_name);
+		$query_str->bindParam("l_name", $l_name);
+
+		// execute the insertion
+		$query_str->execute();
+	} catch(PDOException $e) {
+            $error = "Social Security Number Exists! Failed to add ssn!";
+       	    header('Location: insertPass.php?error='.$error);
+            exit();
+	}
 
 	header('Location: insertPass.php?error='.$error);
 	exit();
