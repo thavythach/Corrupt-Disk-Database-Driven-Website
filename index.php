e56efa01e10007aaa70f1504f78662e28d4358d3
nbigger <nick.bigger@gmail.com>
2018-10-18 21:00:34 -0700

    bug fixes

--- index.php
+++ index.php
@@ -23,9 +23,6 @@
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="~ubuntu">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item active">
               <a class="nav-link" href="showPassengers.php">Show All Passengers <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item active">
