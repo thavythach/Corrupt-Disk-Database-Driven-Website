9df0609d88569524b1947e2d72c7281ae33bcd3e
Thavy Thach <thavythach@gmail.com>
2018-11-14 15:54:09 -0800

    new controllers and etc

--- UpCrate/app/GroupAccess.php
+++ UpCrate/app/GroupAccess.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class GroupAccess extends Model
 {
-    //
+    // table name
+    public $table = 'groupAccess';
+
+    // primary key
+    public $primaryKey = 'group_id';
+
+    public $timestamps = false;
 }
