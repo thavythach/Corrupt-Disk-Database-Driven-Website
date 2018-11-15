ac2308e0c24436aa6928f000b21da3cfde0cbe90
nbigger <nick.bigger@gmail.com>
2018-11-14 17:51:39 -0800

    added user select to file upload

--- UpCrate/resources/views/home.blade.php
+++ UpCrate/resources/views/home.blade.php
@@ -1,28 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<body class="text-center">
+  <script>
+    function yesnoCheck() {
+      if (document.getElementById('privateVis').checked) {
+          document.getElementById('userSelect').style.visibility = 'visible';
+      }
+      else document.getElementById('userSelect').style.visibility = 'hidden';
+    }
+  </script>
+  <h1 class="cover-heading">Get started.</h1>
+  <p class="lead">Upload a file and get started storing with UpCrate.</p>
 
-      <main role="main" class="inner cover">
-        <h1 class="cover-heading">Get started.</h1>
-        <p class="lead">Upload a file and get started storing with UpCrate.</p>
-        <p class="lead">
-          <form action="/process" enctype="multipart/form-data" method="POST">
-            <p>
-                <label for="file">
-                    <input type="file" name="file" id="file"><br>
-                    <input type="radio" name="visibility" value="Private" checked> Private
-                    <input type="radio" name="visibility" value="Public">Public<br>
-                </label>
-            </p>
-            <button>Upload</button>
-          {{ csrf_field() }}
-        </form>
-        </p>
-      </main>
-
-      <footer class="row">
-        @include('includes.footer')
-      </footer>
+  <form action="/process" enctype="multipart/form-data" method="POST">
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="file">Select a file</label>
+        <input type="file" class="form-control-file" name="file" id="file">
+      </div>
+      <div class="form-group col-md-6">
+        <label for="visibility">Visibility</label><br>
+        <input onclick="javascript:yesnoCheck();" class="form-check-input" type="radio" name="visibility" id="privateVis" value="Private">
+        <label class="form-check-label" for="privateVis">Private</label>
+        <br>
+        <input onclick="javascript:yesnoCheck();"class="form-check-input" type="radio" name="visibility" id="publicVis" value="Public" checked>
+        <label class="form-check-label" for="publicVis">Public</label>
+      </div>
     </div>
+    <div id="userSelect"class="form-row" style="visibility:hidden">
+      <div class="form-group col-md-4">
+        <label for="userSelect">Select users that can access: </label>
+        <select multiple class="form-control" id="exampleFormControlSelect2">
+          <option>Jake Redmond</option>
+          <option>Thavy Thach</option>
+          <option>David Chiu</option>
+          <option>Adrian Brody</option>
+          <option>John Johnson</option>
+        </select>
+      </div>
+      <div class="form-group col-md-2"></div>
+    </div>
+    <button class="btn btn-primary btn-lg">Upload</button>
+    {{ csrf_field() }}
+  </form>
+
+  <br><br><br>
+  <center><footer class="row">
+    @include('includes.footer')
+  </footer></center>
+  </div>
 @endsection
